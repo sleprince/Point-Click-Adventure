fbbb72d84793f4a7ddc4b1c97b44cf8fd1a9580e
Stavrin <steffanleprince@gmail.com>
2022-12-19 22:12:07 +0000

    Added DataManager and ItemActions.

---
+++ Assets/Scripts/Actions/ItemActions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemActions : Actions //used to give and receive items during runtime.
+{
+    [SerializeField] ItemDatabase itemDatabase; //drag and drop item database object here in editor.
+    [SerializeField] bool giveItem; //this will decide whether we are giving or receiving the item.
+    [SerializeField] Actions[] yesActions, noActions;
+
+    private Item currentItem;
+
+    public override void Act()
+    {
+		//check if giveItem is true, then give the item
+		
+             //check if we own the item
+ 
+                    //check if the item has the allowMultiple option or not
+ 
+						//check how many items needed, give and substract our item
+
+                    //else give and remove the item
+ 
+            //else receive the item
+    }   
+}
