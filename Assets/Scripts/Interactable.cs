28fbb6173e356eb72435dbf8dfe0a30678b5bb93
Stavrin <steffanleprince@gmail.com>
2023-01-20 21:23:02 +0000

    Better actions depending on cursor for NPCtalk.

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,6 +16,7 @@ public class Interactable : MonoBehaviour
     [SerializeField] InspectAction[] inspectActions;
     [SerializeField] ActivateActions[] activateActions;
     [SerializeField] ItemActions[] itemActions;
+    
 
 
     [SerializeField] float distancePosition = 1f; //how far away from interactable player stops.
@@ -62,26 +65,91 @@ public class Interactable : MonoBehaviour
         player.SetDirection(transform.position);
 
 
+        //string[] action = new string[4] {"InspectAction", "MessageAction", "ActivateActions", "ItemAction"};
 
-        for (int i = 0; i < actions.Length; i++)
-        {
-            actions[i].Act();
+
+       // for (int i = 0; i < actions.Count(); i++)
+        //{
+            //actions[i].Act();
+
+            Actions[] inspect = actions.OfType<InspectAction>().ToArray();
+            Actions[] message = actions.OfType<MessageAction>().ToArray();
+
+            if (_playerScript.I == 0)
+            {
+                for (int j = 0; j < inspect.Count(); j++)
+                {
+                    inspect[j].Act();
+                }
+            }
+
+            if (_playerScript.I == 1)
+            {
+                for (int k = 0; k < message.Count(); k++)
+                {
+                    message[k].Act();
+                }
+            }
+
+
+
+            //}
+
+        /*public enum ArrayNames{
+            InspectAction,
+            MessageAction,
+            ActivateAction,
+            ItemAction
         }
-
+    */
         //0 = Look
         //1 = Talk
         //2 = Use
         //3 = Walk/pick up
+
+
+
+
+       
+
+        //string[] action = new string[4] {"inspectActions", "messageActions", "activateActions", "itemActions"};
+        
+        //string action1 = inspectActions[0].ToString();
+        //action[0].Act();
+        
+        //MethodInfo acts = inspectActions[0].GetType().GetMethod("Act");
+        
+        //acts.Invoke(inspectActions[0], null);
         
         
-        if (_playerScript.I == 0)
+        //$"{action[0]}"[0].
+        
+        /*
+        Debug.Log(action[0]);
+
+        for (int j = 0; j < action.Length; j++)
         {
-            for (int i = 0; i < inspectActions.Length; i++)
+
+            if (_playerScript.I == j)
             {
-                inspectActions[i].Act();
+                for (int i = 0; i < $"{action[j]}".Length; i++)
+                {
+                    //$"{action[j]}"[i].Invoke("")
+                    //inspectActions[i].Act();
+
+                    MethodInfo acts = $"{action[j]}"[i].GetType().GetMethod("Act");
+
+                    acts.Invoke($"{action[j]}"[i], null);
+
+
+
+                }
             }
+
         }
-        
+            */
+    
+        /*
         if (_playerScript.I == 1)
         {
             for (int i = 0; i < messageActions.Length; i++)
@@ -89,7 +157,7 @@ public class Interactable : MonoBehaviour
                 messageActions[i].Act();
             }
         }
-        
+        */
     }
 
     /*
