22b762bbd1dc11603d80daa9ed58e8679abbe58c
Stavrin <steffanleprince@gmail.com>
2023-02-12 13:51:12 +0000

    Removing everything to fix merge conflitct.

--- Assets/Scripts/BaseClass/Actions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Actions : MonoBehaviour
-{
-    public abstract void Act(); //every action derived from the actions class will have the Act() function, which can be different
-    //each time.
-}
