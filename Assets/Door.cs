8fa39579569f02b968f55f988ea6561bce35ab39
Stavrin <steffanleprince@gmail.com>
2023-02-25 16:32:58 +0000

    Win and lose conditions working.

---
+++ Assets/Door.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameManager game;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            game.Win();
+        }
+
+    }
+
+
+}
--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -3,15 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class CameraTrigger : MonoBehaviour
 {
     //CinemachineVirtualCamera mainCam;
     [SerializeField] CinemachineVirtualCamera switchCam;
     [SerializeField] private bool exiting;
+    public GameManager game;
 
     private void Start()
     {
@@ -24,7 +25,13 @@ public class CameraTrigger : MonoBehaviour
         //Debug.Log(name + " colliided with " + other.gameObject.name);
         //collided with
 
-        if (!exiting)
+        Scene mummy = SceneManager.GetActiveScene();
+
+        if (mummy.name == "Mummy")
+            game.Win();
+
+
+            if (!exiting)
         {
             switchCam.Priority = 11;
             exiting = true;
